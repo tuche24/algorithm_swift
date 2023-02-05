a9ea0f05bc8a309d48d4ecc47f4580538dd9f43b
tuche <tuche24@naver.com>
2023-02-06 06:15:02 +0900

    Fibo 수열 수정

--- MyPlayground.playground/Pages/Fibo.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Fibo.xcplaygroundpage/Contents.swift
@@ -12,22 +12,30 @@ var fibo: [Int] = [Int]()
 
 func DFS(_ n: Int) -> Int {
     if n == 1 {
-        fibo.insert(1, at: 1)
-        return 1
+        fibo[1] = 1
+        return fibo[1]
     } else if n == 2 {
-        fibo.insert(1, at: 2)
-        return 1
+        fibo[2] = 1
+        return fibo[2]
     } else {
-        fibo.insert(DFS(n-2) + DFS(n-1), at: n)
-        return DFS(n-2) + DFS(n-1)
+        fibo[n] = DFS(n-2) + DFS(n-1)
+        return fibo[n]
     }
 }
 
-DFS(10)
-
-for i in 1...10 {
-    print(fibo[i], terminator: " ")
+func solve(variable: Int) {
+    fibo = []
+    for _ in 0...variable {
+        fibo.append(0)
+    }
+    DFS(variable)
+    for i in 1...10 {
+        print(fibo[i], terminator: " ")
+    }
 }
+
+solve(variable: 10)
+
 
 
 
