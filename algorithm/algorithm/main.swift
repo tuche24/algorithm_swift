a88c7a3e4e3b2a198ec2e22542472a08c262a463
tuche <tuche24@naver.com>
2023-02-14 22:22:59 +0900

    Node Class 추가

--- algorithm/algorithm/main.swift
+++ algorithm/algorithm/main.swift
@@ -10,3 +10,13 @@ import Foundation
 let a = readLine()
 print(a)
 
+public class BinaryNode<Element> {
+
+  public var value: Element
+  public var leftChild: BinaryNode?
+  public var rightChild: BinaryNode?
+
+  public init(value: Element) {
+    self.value = value
+  }
+}
